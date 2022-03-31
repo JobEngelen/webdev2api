364e2b9f4f5dd8be393564c9faffd582d922ed1b
Job Engelen <jobengelen@yahoo.nl>
2022-03-31 14:38:00 +0200

    Third commit

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,6 +1,13 @@
 <?php
-$type = "mysql";
+/*$type = "mysql";
 $servername = "mysql";
 $username = "root";
 $password = "secret123";
-$database = "developmentdb";
+$database = "developmentdb";*/
+
+
+$type = "mysql";
+$servername = "eu-cdbr-west-02.cleardb.net";
+$username = "b042b277652ffd";
+$password = "15e6f053";
+$database = "heroku_90eb7c56d939b43";
