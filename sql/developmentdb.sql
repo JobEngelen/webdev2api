1254578f40e7b7c92264af2acc22f8b89f2c2b15
Job Engelen <jobengelen@yahoo.nl>
2022-06-13 17:11:50 +0200

    Updated database export

--- sql/developmentdb.sql
+++ sql/developmentdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Gegenereerd op: 31 mrt 2022 om 12:21
+-- Gegenereerd op: 13 jun 2022 om 15:11
 -- Serverversie: 10.6.5-MariaDB-1:10.6.5+maria~focal
 -- PHP-versie: 7.4.27
 
@@ -39,6 +39,59 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`id`, `name`) VALUES
 (1, 'wasdroger'),
 (3, 'wasmachine');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `ordertime` datetime NOT NULL DEFAULT current_timestamp(),
+  `userid` int(11) NOT NULL,
+  `delivered` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `order`
+--
+
+INSERT INTO `order` (`id`, `ordertime`, `userid`, `delivered`) VALUES
+(42, '2022-06-13 14:38:48', 2, 1),
+(43, '2022-06-13 14:38:52', 2, 0),
+(44, '2022-06-13 14:39:19', 2, 0),
+(45, '2022-06-13 15:00:16', 2, 0),
+(46, '2022-06-13 15:10:07', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `order_product`
+--
+
+CREATE TABLE `order_product` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `order_product`
+--
+
+INSERT INTO `order_product` (`order_id`, `product_id`, `quantity`) VALUES
+(42, 6, 1),
+(42, 7, 1),
+(43, 6, 1),
+(43, 11, 3),
+(44, 6, 4),
+(44, 7, 1),
+(44, 10, 3),
+(44, 11, 3),
+(45, 6, 1),
+(46, 6, 1),
+(46, 7, 1);
 
 -- --------------------------------------------------------
 
@@ -86,8 +139,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `email`, `userType`) VALUES
-(1, 'username', '$2y$10$DQlV0u9mFmtOWsOdxXX9H.4kgzEB3E8o97s.S.Pdy4klUAdBvtVh.', 'username@password.com', 0),
-(2, 'admin', '$2y$10$RxPfZXVxG0JEXDlfSh3bQOlhBduQLk/PCG5H6ttbBpNHtC.TE7e4q', 'admin@email.com', 1);
+(1, 'guest', '$2y$10$DQlV0u9mFmtOWsOdxXX9H.4kgzEB3E8o97s.S.Pdy4klUAdBvtVh.', 'username@password.com', 0),
+(2, 'admin', '$2y$10$RxPfZXVxG0JEXDlfSh3bQOlhBduQLk/PCG5H6ttbBpNHtC.TE7e4q', 'admin@email.com', 1),
+(3, 'user', '$2y$10$DQlV0u9mFmtOWsOdxXX9H.4kgzEB3E8o97s.S.Pdy4klUAdBvtVh.', 'user@user.com', 0);
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -98,6 +152,20 @@ INSERT INTO `user` (`id`, `username`, `password`, `email`, `userType`) VALUES
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexen voor tabel `order_product`
+--
+ALTER TABLE `order_product`
+  ADD KEY `order_product_ibfk_1` (`order_id`),
+  ADD KEY `order_product_ibfk_2` (`product_id`);
 
 --
 -- Indexen voor tabel `product`
@@ -124,6 +192,12 @@ ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
+-- AUTO_INCREMENT voor een tabel `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+
+--
 -- AUTO_INCREMENT voor een tabel `product`
 --
 ALTER TABLE `product`
@@ -133,11 +207,17 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT voor een tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Beperkingen voor geëxporteerde tabellen
 --
+
+--
+-- Beperkingen voor tabel `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `order_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
 
 --
 -- Beperkingen voor tabel `product`
